766f2abc032c8caaa3181994fcabbb0db03f01fb
Samiksha Fulambarkar <samikshafulambarkar99@gmail.com>
2023-05-18 14:25:46 +0530

    Add project files.

---
+++ Nimap_Infotech_Project/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Nimap_Infotech_Project.Models
+{
+    [Table("product")]
+    public class Product
+    {
+        [Key]
+        [ScaffoldColumn(false)]
+        public int ProductId { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string? ProductName { get; set; }
+        public int CategoryId { get; set; }
+        [NotMapped]
+        public string? CategoryName { get; set; }
+    }
+}
---
+++ Nimap_Infotech_Project/Views/Shared/_Layout.cshtml.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Nimap_Infotech_Project.Views.Shared
+{
+    public class _Layout.cshtmlModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
